315840495958fb0d6eac5ca89bc432970c298632
Lin Hong <llinhong.2301+github@gmail.com>
2021-11-20 15:17:52 -0500

    Server: setup CORS
    Add cors as dependency

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 import router from './routes';
 
@@ -16,6 +17,7 @@ async function main() {
     });
 
     const app = express();
+    app.use(cors());
     app.use(express.json());
 
     app.use('/', router);
