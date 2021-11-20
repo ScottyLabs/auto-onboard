3836dc397a7219991e2f82642370647ec57a923a
Lin Hong <llinhong.2301+github@gmail.com>
2021-11-20 15:15:36 -0500

    Client: Add user feedback upon submission
    - Dialogs for unsuccessful and successful API calls will now appear.
    - Fix React complaining about uncontrolled inputs changing to
      controlled.

--- client/src/routes/Form.jsx
+++ client/src/routes/Form.jsx
@@ -3,6 +3,11 @@ import '../App.css';
 import {
     Button,
     Checkbox,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
     FormControl,
     Grid,
     ListItemText,
@@ -11,6 +16,7 @@ import {
     Typography,
 } from '@mui/material';
 import React from 'react';
+import { LoadingButton } from '@mui/lab';
 
 const pref = {
     tech: 'Tech',
@@ -18,7 +24,15 @@ const pref = {
     general: 'General Planning',
 };
 
+const responseStates = {
+    NONE: 0,
+    LOADING: 1,
+    ERROR: -1,
+    SUCCESS: 2,
+};
+
 const BACKEND = 'http://localhost:3000';
+const EMAIL = 'tech@scottylabs.org';
 
 function Form() {
     const [name, setName] = React.useState();
@@ -29,6 +43,10 @@ function Form() {
 
     const [committeePref, setCommitteePref] = React.useState([]);
     const [prefError, setPrefError] = React.useState(false);
+
+    const [responseStatus, setResponseStatus] = React.useState(
+        responseStates.NONE
+    );
 
     const validateName = (name) => {
         const hasError = (name || '').length === 0;
@@ -72,96 +90,152 @@ function Form() {
             validateCommittee(committeePref)
         ) {
             try {
-                const response = await axios.post(`${BACKEND}/submit`, {
+                setResponseStatus(responseStates.LOADING);
+                await axios.post(`${BACKEND}/submit`, {
                     name,
                     andrew: (andrew || '').toLowerCase(),
                     committeePref,
                 });
-                console.log(response);
+                setResponseStatus(responseStates.SUCCESS);
             } catch (e) {
-                console.error(e);
+                setResponseStatus(responseStates.ERROR);
             }
         }
     };
 
+    const handleDialogClose = () => {
+        setResponseStatus(responseStates.NONE);
+    };
+
     return (
-        <FormControl component="form" fullWidth>
-            <Grid container spacing={2} marginTop={2}>
-                <Grid item xs={12}>
-                    <Typography variant="h4">ScottyLabs Onboarding</Typography>
+        <React.Fragment>
+            <FormControl component="form" fullWidth>
+                <Grid container spacing={2} marginTop={2}>
+                    <Grid item xs={12}>
+                        <Typography variant="h4">
+                            ScottyLabs Onboarding
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            variant="outlined"
+                            label="Name"
+                            fullWidth
+                            required
+                            value={name || ''}
+                            onChange={onNameChange}
+                            error={nameError}
+                            helperText={
+                                nameError ? 'Name cannot be blank.' : undefined
+                            }
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            variant="outlined"
+                            label="Andrew ID"
+                            fullWidth
+                            required
+                            value={andrew || ''}
+                            onChange={onAndrewChange}
+                            error={andrewError}
+                            helperText={
+                                andrewError
+                                    ? 'Andrew ID must be valid.'
+                                    : undefined
+                            }
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            select
+                            variant="outlined"
+                            label="Committee Preferences"
+                            value={committeePref}
+                            onChange={onCommitteeChange}
+                            fullWidth
+                            required
+                            error={prefError}
+                            helperText={
+                                prefError
+                                    ? 'You must select a committee'
+                                    : 'You may select multiple committees'
+                            }
+                            inputProps={{
+                                multiple: true,
+                                renderValue: (s) =>
+                                    s.map((x) => pref[x]).join(', '),
+                            }}
+                        >
+                            {Object.entries(pref).map(([key, val]) => (
+                                <MenuItem key={key} value={key}>
+                                    <Checkbox
+                                        checked={
+                                            committeePref.indexOf(key) > -1
+                                        }
+                                    />
+                                    <ListItemText primary={val} />
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant="body2">
+                            By submitting this form, you consent to be added to
+                            the ScottyLabs mailing list.
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <LoadingButton
+                            loading={responseStatus === responseStates.LOADING}
+                            variant="contained"
+                            onClick={onSubmit}
+                        >
+                            Submit
+                        </LoadingButton>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        variant="outlined"
-                        label="Name"
-                        fullWidth
-                        required
-                        value={name}
-                        onChange={onNameChange}
-                        error={nameError}
-                        helperText={
-                            nameError ? 'Name cannot be blank.' : undefined
-                        }
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        variant="outlined"
-                        label="Andrew ID"
-                        fullWidth
-                        required
-                        value={andrew}
-                        onChange={onAndrewChange}
-                        error={andrewError}
-                        helperText={
-                            andrewError ? 'Andrew ID must be valid.' : undefined
-                        }
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        select
-                        variant="outlined"
-                        label="Committee Preferences"
-                        value={committeePref}
-                        onChange={onCommitteeChange}
-                        fullWidth
-                        required
-                        error={prefError}
-                        helperText={
-                            prefError
-                                ? 'You must select a committee'
-                                : 'You may select multiple committees'
-                        }
-                        inputProps={{
-                            multiple: true,
-                            renderValue: (s) =>
-                                s.map((x) => pref[x]).join(', '),
-                        }}
-                    >
-                        {Object.entries(pref).map(([key, val]) => (
-                            <MenuItem key={key} value={key}>
-                                <Checkbox
-                                    checked={committeePref.indexOf(key) > -1}
-                                />
-                                <ListItemText primary={val} />
-                            </MenuItem>
-                        ))}
-                    </TextField>
-                </Grid>
-                <Grid item xs={12}>
-                    <Typography variant="body2">
-                        By submitting this form, you will be added to the
-                        ScottyLabs mailing list.
-                    </Typography>
-                </Grid>
-                <Grid item xs={12}>
-                    <Button variant="contained" onClick={onSubmit}>
-                        Submit
-                    </Button>
-                </Grid>
-            </Grid>
-        </FormControl>
+            </FormControl>
+            <Dialog
+                open={responseStatus === responseStates.ERROR}
+                onClose={handleDialogClose}
+            >
+                <DialogTitle>Error!</DialogTitle>
+                <DialogContent>
+                    <DialogContentText mb={2}>
+                        We could not connect the ScottyLabs server. Check your
+                        internet connection, and try again.
+                    </DialogContentText>
+                    <DialogContentText>
+                        If problems persist, please let us know at {EMAIL}.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleDialogClose}>Close</Button>
+                </DialogActions>
+            </Dialog>
+            <Dialog
+                open={responseStatus === responseStates.SUCCESS}
+                onClose={handleDialogClose}
+            >
+                <DialogTitle>Success!</DialogTitle>
+                <DialogContent>
+                    <DialogContentText mb={2}>
+                        We've received your application. You will receive a
+                        confirmation email shortly, together with a link to our
+                        Slack channel. You will also be added to our mailing
+                        list.
+                    </DialogContentText>
+                    <DialogContentText>
+                        In the meantime, if you have any questions, please let
+                        us know on Slack, or contact us at {EMAIL}.
+                    </DialogContentText>
+                    <DialogActions>
+                        <Button onClick={handleDialogClose}>Continue</Button>
+                    </DialogActions>
+                </DialogContent>
+            </Dialog>
+        </React.Fragment>
     );
 }
 
